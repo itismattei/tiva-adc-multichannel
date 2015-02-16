6dab1245763e3fb37cdd6995478614b58444bdaa
massimo <massimo@localhost>
2015-02-16 23:06:01 +0100

    Lettura canali 2-3-8 (PE.1 - PE.0 - PE.5)

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "driverlib/adc.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/adc.h"
-#include "inc/hw_types.h"
 #include "driverlib/debug.h"
 
 /*main(void) {
